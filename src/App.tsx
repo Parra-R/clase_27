b9e59c1941ecf02f68c728c3e26abcebd0197288
Daniel Parra Riera <danielparrariera7@gmail.com>
2026-03-09 21:40:31 +0100

    Add routing and new pages: implement navigation and create Home and Contact components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
-import TablaPedidos from './components/TablaPedidos/TablaPedidos'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import { Contact } from './Pages/Contact/Contact'
 
 function App() {
   return (
-    
-    <TablaPedidos/>
+    <BrowserRouter>
+
+      {/* 1. La Navbar (Siempre visible) */}
+      <nav className="p-4 bg-indigo-600 text-white flex gap-4 shadow-lg">
+        <Link to="/" className="hover:underline font-bold">Inicio</Link>
+        <Link to="/contacto" className="hover:underline font-bold">Contacto</Link>
+      </nav>
+
+      {/* 2. El Área de Contenido (Aquí cambian los componentes) */}
+      <div className="p-10">
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+        /* 404 - Si no encuentra la ruta */
+        <Route path='*' element={<h2 className='text-red-500'>404 - Not Found</h2>}></Route>
+        </Routes>
+      </div>
+
+    </BrowserRouter>
   )
 }
+
 
 export default App
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1 className='text-2xl font-bold'>Bienvenida a casa</h1>
+  )
+}
+
+export default Home
